659efce9969d2fb02704c45c85c266330410a6f1
Roberto Rodrigues <Roberto.Rodrigues@smith-nephew.com>
2022-02-01 12:27:10 +0000

    Completed example 6 - Fixtures

--- tests/test_accum.py
+++ tests/test_accum.py
@@ -3,40 +3,42 @@
 # Imports
 #----------------------------------------------------------
 
-from itertools import accumulate
-from os import access
 import pytest
 from stuff.accum import Accumulator
+
+
+#----------------------------------------------------------
+# Fixtures
+#----------------------------------------------------------
+
+@pytest.fixture
+def accum():
+    return Accumulator()
 
 #----------------------------------------------------------
 # Tests for the Accumulator class
 #----------------------------------------------------------
 
-def test_accumulator_init():
-    accum = Accumulator()
+def test_accumulator_init(accum):
     assert accum.count == 0 
 
 
-def test_accumulator_add_one():
-    accum = Accumulator()
+def test_accumulator_add_one(accum):
     accum.add()
     assert accum.count == 1
 
 
-def test_accumulator_add_three():
-    accum = Accumulator()
+def test_accumulator_add_three(accum):
     accum.add(3)
     assert accum.count == 3
 
 
-def test_accumulator_add_twice():
-    accum = Accumulator()
+def test_accumulator_add_twice(accum):
     accum.add()
     accum.add()
     assert accum.count == 2
 
-def test_accumulator_add_in_loop():
-    accum = Accumulator()
+def test_accumulator_add_in_loop(accum):
 
     numbers = [3, 3, 3, 9, 2]
 
@@ -46,7 +48,7 @@ def test_accumulator_add_in_loop():
     assert accum.count == 20        # Adding all the numbers in the list will result in a sum of 20
 
 
-def test_accumulator_cannot_set_count_directly():
-    accum = Accumulator()
+def test_accumulator_cannot_set_count_directly(accum):
     with pytest.raises(AttributeError, match=r"can't set attribute") as e:
         accum.count = 10
+
