522e5c05a15b1277ec261b4a4a9a49f2e3c24422
Roberto Rodrigues <Roberto.Rodrigues@smith-nephew.com>
2022-02-01 10:46:04 +0000

    Completed the parameters example

--- tests/test_math.py
+++ tests/test_math.py
@@ -32,3 +32,29 @@ def test_divide_by_zero():
 
     assert 'division by zero' in str(e.value)
 
+#----------------------------------------------------------
+# DRY Principle: Don't Repeat Yourself
+# A parametrized test function
+#----------------------------------------------------------
+
+# list of tuples to be used as arguments to be passed to the test function
+
+products = [
+    (2, 3, 6),                  # positive integers
+    (1, 99, 99),                # identity
+    (0, 99, 0),                 # zero
+    (3, -4, -12),               # + by -
+    (-5, -4, 20),              # - by -
+    (2.5, 6.7, 16.75),          # floats
+]
+
+@pytest.mark.parametrize('a, b, product', products)
+def test_multiplication(a, b, product):
+    assert a* b == product
+
+
+
+
+
+
+
